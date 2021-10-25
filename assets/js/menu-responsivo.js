551a60c338ebd17b1c4eca22d64080b034efabb0
Thalya Stéffany <87552041+ThalyaSteffany-tech@users.noreply.github.com>
2021-10-24 22:19:38 -0300

    Update menu-responsivo.js

--- assets/js/menu-responsivo.js
+++ assets/js/menu-responsivo.js
@@ -2,7 +2,7 @@
     const btnMenuResponsivo  = document.querySelector('[data-btnResponsivo]')
     .addEventListener('click',menuResponsivo)
     function menuResponsivo() {
-        let navUl = document.querySelector('#navUl')
+        let navUl = document.querySelector('[data-navUl]')
         navUl.classList.toggle('mostrar')
     }
 })()
